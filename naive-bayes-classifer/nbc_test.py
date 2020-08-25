737e3cdc54eb350d95c5148bbfa3348634381bca
LuisFelipeUrena <60860402+LuisFelipeUrena@users.noreply.github.com>
2020-08-25 14:01:06 -0400

    added a sklearn model to the test just for sanity check

--- naive-bayes-classifer/nbc_test.py
+++ naive-bayes-classifer/nbc_test.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import datasets
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
 
 from nbc import NaiveBayesClassifier
 
@@ -15,4 +16,15 @@ nb.fit(X_train,y_train)
 predictions = nb.predict(X_test)
 acc = accuracy_score(y_test,predictions)
 
-print(f'Naive Bayes Classification acurracy is: {acc}')
+
+
+print(f'By hand Naive Bayes Classification acurracy is: {acc}')
+
+# here we implement the GaussianNB which is built in Sklearn library, just for comparison
+gnb = GaussianNB()
+gnb.fit(X_train,y_train)
+gnb_pred = gnb.predict(X_test)
+acc_2 = accuracy_score(y_test,gnb_pred)
+
+print(f'Scikit Learn Gaussian Naive Bayes Classification acurracy is: {acc_2}')
+
