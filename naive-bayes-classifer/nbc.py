fdc7ce01aa1dc02e5cee7c8739f2c5085622553e
LuisFelipeUrena <60860402+LuisFelipeUrena@users.noreply.github.com>
2020-08-26 14:55:45 -0400

    changed the dataset on the test file

--- naive-bayes-classifer/nbc.py
+++ naive-bayes-classifer/nbc.py
@@ -3,25 +3,29 @@ import scipy as scy
 
 class NaiveBayesClassifier:
     '''
+    By hand implementation of Naive Bayes Classifier
 
     '''
     def fit(self,x,y):
         '''
         input: 
             x: array-like, features to be used in the model
-            y: target variable
+            y: 1d array target variable
         returns:
-
-
-
-
-
+            a trained model able to produce predictions based
+            on the training data it was given.
+        example:
+            >>> from nbc import NaiveBayesClassifier
+            >>> X, y = dataset.data, dataset.target
+            >>> nb = NaiveBayesClassifer()
+            >>> nb.fit(X, y)
         '''
         # get the prior probability
         # get the mean and variance of each class
         n_samples, n_features = x.shape
         self._classes = np.unique(y) # identifies unique classes
         n_classes = len(self._classes) # number of classes
+        self.feature_amount = x.shape[1]
         
         self._mean = np.zeros((n_classes, n_features), dtype=np.float64) # mean
         self._var = np.zeros((n_classes, n_features), dtype=np.float64) #variance
@@ -35,6 +39,12 @@ class NaiveBayesClassifier:
             self._prior[i] = x_classes.shape[0] / float(n_samples)
 
     def predict(self,x):
+        '''
+        input:
+            x: array-like, it must be the same dimension as the original training data
+        returns:
+            an array with the predictions of each observation    
+        '''
         y_pred = [self._predict(i) for i in x]
         return np.array(y_pred)
     
--- naive-bayes-classifer/nbc_test.py
+++ naive-bayes-classifer/nbc_test.py
@@ -1,14 +1,17 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn import datasets
+from sklearn.datasets import load_wine
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import GaussianNB
 
 from nbc import NaiveBayesClassifier
 
-X, y = datasets.make_classification(n_samples=1000, n_features =10, n_classes=2,random_state=42)
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+# in this case we will use the Wine dataset provided by the sklearn library
+wine = load_wine()
+
+X, y = wine.data, wine.target
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=128)
 
 nb = NaiveBayesClassifier()
 
